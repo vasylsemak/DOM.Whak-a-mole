e93cced4999c6f0f1c542f232895000b03123b8b
Vasyl Semak <vasylsmk1401@gmail.com>
2020-08-31 13:27:15 -0400

    add prize message

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 let score = 0;
+const screenScore = document.getElementById('score');
 const holes = Array.from(document.getElementsByClassName('hole'));
 
-function moleToggler() {
-  let randomHole = Math.floor(Math.random() * holes.length);
+const moleToggler = () => {
+  const randomHole = Math.floor(Math.random() * holes.length);
   holes[randomHole].classList.toggle('mole');
-}
-
-const runMoleToggler = () => setInterval(moleToggler, 100);
-
+};
+const runMoleToggler = () => setInterval(moleToggler, 200);
 runMoleToggler();
+
+document.getElementById('whack-a-mole').addEventListener('click', event => {
+  if (event.target.classList.contains('mole')) {
+    event.target.classList.remove('mole');
+    score++;
+    screenScore.innerText = score;
+  }
+  if (score === 6) {
+    let won = document.getElementById('won');
+    won.innerText = 'YOU WON A PRIZE!!!';
+    // bodyO.appendChild(uWin).innerText = 'YOU WON A PRIZE!!!';
+
+    setTimeout(() => {
+      score = 0;
+      screenScore.innerText = score;
+      won.innerText = '';
+    }, 2000);
+  }
+});

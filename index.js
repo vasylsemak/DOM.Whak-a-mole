ea3abbeebc8cd74b24588012fdf4ac07354ffefd
Vasyl Semak <vasylsmk1401@gmail.com>
2020-08-31 11:25:36 -0400

    add setInterval, mmole appears consistantly

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 let score = 0;
+const holes = Array.from(document.getElementsByClassName('hole'));
+
+function moleToggler() {
+  let randomHole = Math.floor(Math.random() * holes.length);
+  holes[randomHole].classList.toggle('mole');
+}
+
+const runMoleToggler = () => setInterval(moleToggler, 100);
+
+runMoleToggler();
